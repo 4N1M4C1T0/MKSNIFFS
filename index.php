645f8385f1c746aef5b844d6957bfd14828afda2
Richard Ernesto Meneses Ferrer <richardmeneses2110@gmail.com>
2024-03-11 03:08:23 -0500

    Avance

--- index.php
+++ index.php
@@ -1,11 +1,57 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://unpkg.com/pageable@latest/dist/pageable.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/629fd9ff7b.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/styles.css">
+        <title>MK-SNIFFS</title>
+    </head>
+    <body>
+        <div id="container">
+            <div data-anchor="Page 1" class="bg-success">
+                <div class="logos">
+                    <a href="https://www.gob.pe/midagri" target="_blank">
+					    <img src="img/logo_ministerio.jpg" class="logo-ministerio" height="40px" alt="Ministerio de Agricultura y Riego">
+				    </a>
+                    <a href="https://www.serfor.gob.pe" target="_blank">
+					    <img src="img/serfor-1-1.png" class="logo-serfor" alt="">
+				    </a>
+                </div>
+                <div class="barra">
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="#" onclick="#" class="ico-line" title="Contacto">
+                                    <i class="fa-solid fa-envelope"></i>
+					            </a>
+                            </li>
+                            <li>
+                                <a href="#" class="ico-line" title="Idioma" onclick="#">
+                                    <i class="fa-solid fa-globe"></i>
+					            </a>
+                            </li>
+                            <li>
+                                <a href="#" data-src="#" data-fancybox="" class="ico-line" title="Iniciar Sesión">
+                                    <i class="fa-solid fa-user"></i>
+					            </a>
+                            </li>
+                        </ul>
+                    </nav>					
+                </div>
+            </div>
+            <div data-anchor="Page 2" class="bg-primary">
+                <h1>LALALA</h1>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/pageable@latest/dist/pageable.min.js"></script>
+        <script src="js/pageable.js"></script>
+    </body>
 </html>
