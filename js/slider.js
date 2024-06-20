2bf55e2455604bf2bc0a2f7ddf0189f01e93fb0a
Richard Ernesto Meneses Ferrer <richardmeneses2110@gmail.com>
2024-06-20 11:34:40 -0500

    cambios
    se realizo cambios

--- js/slider.js
+++ js/slider.js
@@ -1,56 +1,37 @@
-//creacion de variables de tipo let
-
 let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let mniniaturas = document.querySelectorAll('.miniatura .item');
+let ccircles = document.querySelectorAll('.circle .item');
 
 // configurar paramentros
 let countItem = items.length;
 let itemActive = 0;
 
-// Evento click siguiente
-next.onclick = function(){
-    itemActive = itemActive + 1;
-    if(itemActive >= countItem){
-        itemActive = 0;
-    }
-    showSlider();
-}
-// Evento click anterior
-prev.onclick = function(){
-    itemActive = itemActive - 1;
-    if(itemActive < 0){
-        itemActive = countItem - 1;
-    }
-    showSlider();
-}
 //auto run slider
-let refreshIterval = setInterval(() => {
-    next.click();
-}, 15000)
+let refreshInterval = setInterval(() => {
+    itemActive = (itemActive + 1) % countItem;
+    showSlider();
+}, 15000);
+
 function showSlider(){
     //remove item active old
     let itemActiveOld = document.querySelector('.slider .list .item.active');
-    let mniniaturaActiveOld = document.querySelector('.miniatura .item.active');
+    let ccircleActiveOld = document.querySelector('.circle .item.active');
     itemActiveOld.classList.remove('active');
-    mniniaturaActiveOld.classList.remove('active');
+    ccircleActiveOld.classList.remove('active');
 
     // active new item
     items[itemActive].classList.add('active');
-    mniniaturas[itemActive].classList.add('active');
-
-    //clear auti time run slider
-    clearInterval(refreshIterval);
-    refreshIterval = setInterval(() => {
-        next.click();
-    }, 15000)
+    ccircles[itemActive].classList.add('active');
 }
 
-// click miniatura
-mniniaturas.forEach((mniniatura, index) => {
-    mniniatura.addEventListener('click', () => {
+// click circle
+ccircles.forEach((ccircle, index) => {
+    ccircle.addEventListener('click', () => {
         itemActive = index;
         showSlider();
-    })
-})
+        clearInterval(refreshInterval);
+        refreshInterval = setInterval(() => {
+        itemActive = (itemActive + 1) % countItem;
+        showSlider();
+    }, 15000);
+    });
+});
