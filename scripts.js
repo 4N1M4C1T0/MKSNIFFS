ca0ead5b2c4dcf725bc4cc7595b5ade4e3e3f202
Andypm16 <andy162001@hotmail.com>
2024-07-01 15:15:19 -0500

    vista infografia funcional
    funciona el codigo

--- scripts.js
+++ scripts.js
@@ -1,32 +1,10 @@
-document.querySelectorAll('.dropdown-content a').forEach(function(element) {
-    element.addEventListener('click', function(event) {
-        event.preventDefault();
-        var option = event.target.getAttribute('data-option');
-        var text = '';
-
-        switch(option) {
-            case '1':
-                text = 'Has seleccionado la Ecozona Costa.';
-                break;
-            case '2':
-                text = 'Has seleccionado la Ecozona Sierra.';
-                break;
-            case '3':
-                text = 'Has seleccionado la Ecozona Selva Alta Accesible.';
-                break;
-            case '4':
-                text = 'Has seleccionado la Ecozona Selva Alta de Difícil Acceso.';
-                break;
-            case '5':
-                text = 'Has seleccionado la Ecozona Selva Baja.';
-                break;
-            case '6':
-                text = 'Has seleccionado la Ecozona Hidromórfica.';
-                break;
-            default:
-                text = 'Ninguna opción seleccionada.';
-        }
-
-        document.getElementById('selected-text').innerText = text;
-    });
-});
+function cambiarTexto() {
+    var seleccion = document.getElementById("opciones").value;
+    var texto = document.getElementById("texto");
+    
+    if (seleccion === "costa") {
+        texto.innerHTML = "15 025 311";
+    } else if (seleccion === "sierra") {
+        texto.innerHTML = "999 999 999";
+    }
+}
