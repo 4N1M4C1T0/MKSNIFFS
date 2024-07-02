2bfb4c48d3af2397fe7ca946c9e68422979d0bc0
Andypm16 <andy162001@hotmail.com>
2024-07-02 12:07:35 -0500

    infografia funcional completo
    completo

---
+++ script.js
@@ -0,0 +1,38 @@
+function cambiarTexto() {
+    var seleccion = document.getElementById("opciones").value;
+    var texto1 = document.getElementById("texto1");
+    var texto2 = document.getElementById("texto2");
+    var texto3 = document.getElementById("texto3");
+    var texto4 = document.getElementById("texto4");
+    var texto5 = document.getElementById("texto5");
+    var texto6 = document.getElementById("texto6");
+    var imagen = document.getElementById("imagen");
+    var texto_superior = document.getElementById("texto-superior");
+    var titulo = document.getElementById("titulo");
+    
+    if (seleccion === "opcion1") {
+        texto1.innerHTML = "100";
+        texto2.innerHTML = "200";
+        texto3.innerHTML = "300";
+        texto4.innerHTML = "400";
+        texto5.innerHTML = "500";
+        texto6.innerHTML = "600";
+        imagen.src = "./img/fondo-agua.jpg";
+        imagen.alt = "Descripción de la imagen";
+        texto_superior.innerHTML = "Ecozona";
+        titulo.innerHTML = "Costa";
+
+    } else if (seleccion === "opcion2") {
+        texto1.innerHTML = "700";
+        texto2.innerHTML = "800";
+        texto3.innerHTML = "900";
+        texto4.innerHTML = "1000";
+        texto5.innerHTML = "1100";
+        texto6.innerHTML = "1200";
+        imagen.src = "./img/serfor-1-1.png";
+        imagen.alt = "Descripción de la imagen";
+        texto_superior.innerHTML = "Ecozona";
+        titulo.innerHTML = "Sierra";
+
+    }
+}
