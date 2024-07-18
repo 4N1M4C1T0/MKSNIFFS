f36d1b312d7f5f8e78d688f02c2db370ae164d7c
Andypm16 <andy162001@hotmail.com>
2024-07-18 15:44:37 -0500

    avance
    avance

--- js/resultados/1.js
+++ js/resultados/1.js
@@ -1,22 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const optionSelect = document.getElementById('optionSelect');
+$(document).ready(function() {
+    // Inicializar select2
+    $('#mySelect').select2({
+        dropdownAutoWidth: true, /* Permite el ajuste automático del ancho del desplegable */
+        width: '100px' /* Ancho del select */
+    });
+
+    const optionSelect = document.getElementById('mySelect');
     const contentImage = document.getElementById('contentImage');
     const contentH1 = document.getElementById('contentH1');
     const contentH2 = document.getElementById('contentH2');
-    const contentH3_1 = document.getElementById('contentH3_1');
-    const contentH3_2 = document.getElementById('contentH3_2');
-    const contentP_1 = document.getElementById('contentP_1');
+    const contenedorInferior = document.getElementById('contenedorInferior');
     const contentSpan1 = document.getElementById('contentSpan1');
     const contentSpan2 = document.getElementById('contentSpan2');
-    const contenedorInferior = document.getElementById('contenedorInferior');
 
     const contentData = {
         option1: {
             imgSrc: '../../img/vistas/politica_nacional.jpg',
             h1Text: 'Título 1',
             h2Text: 'Subtítulo 1',
-            h3Text1: ['Encabezado 1.1', 'Encabezado 1.2', 'Encabezado 1.3', 'Encabezado 1.4','Encabezado 1.5', 'Encabezado 1.6'],
-            h3Text2: ['Encabezado 1.1.1', 'Encabezado 1.2.2', 'Encabezado 1.3.3', 'Encabezado 1.4.4','Encabezado 1.5.5', 'Encabezado 1.6.6'],
+            h3Text1: ['Encabezado 1.1', 'Encabezado 1.2', 'Encabezado 1.3', 'Encabezado 1.4', 'Encabezado 1.5', 'Encabezado 1.6'],
+            h3Text2: ['Encabezado 1.1.1', 'Encabezado 1.2.2', 'Encabezado 1.3.3', 'Encabezado 1.4.4', 'Encabezado 1.5.5', 'Encabezado 1.6.6'],
             pText: ['Parrafo 1.1', 'Parrafo 1.2', 'Parrafo 1.3', 'Parrafo 1.4', 'Parrafo 1.5', 'Parrafo 1.6'],
             spanText1: 'Fuente: DGGCFFS - SERFOR ©',
             spanText2: 'Datos 2024'
@@ -25,8 +28,8 @@ document.addEventListener("DOMContentLoaded", function() {
             imgSrc: '../../img/vistas/politica_nacional.jpg',
             h1Text: 'Título 2',
             h2Text: 'Subtítulo 2',
-            h3Text1: ['Encabezado 2.1', 'Encabezado 2.2', 'Encabezado 2.3', 'Encabezado 2.4','Encabezado 2.5', 'Encabezado 2.6'],
-            h3Text2: ['Encabezado 2.1.1', 'Encabezado 2.2.2', 'Encabezado 2.3.3', 'Encabezado 2.4.4','Encabezado 2.5.5', 'Encabezado 2.6.6'],
+            h3Text1: ['Encabezado 2.1', 'Encabezado 2.2', 'Encabezado 2.3', 'Encabezado 2.4', 'Encabezado 2.5', 'Encabezado 2.6'],
+            h3Text2: ['Encabezado 2.1.1', 'Encabezado 2.2.2', 'Encabezado 2.3.3', 'Encabezado 2.4.4', 'Encabezado 2.5.5', 'Encabezado 2.6.6'],
             pText: ['Parrafo 2.1', 'Parrafo 2.2', 'Parrafo 2.3', 'Parrafo 2.4', 'Parrafo 2.5', 'Parrafo 2.6'],
             spanText1: 'Fuente: DGGCFFS - SERFOR ©',
             spanText2: 'Datos 2024'
@@ -38,36 +41,31 @@ document.addEventListener("DOMContentLoaded", function() {
         contentImage.src = data.imgSrc;
         contentH1.textContent = data.h1Text;
         contentH2.textContent = data.h2Text;
-        
-        const items = contenedorInferior.getElementsByClassName('item');
-        for (let i = 0; i < items.length; i++) {
-            const h3_1 = items[i].querySelector('.subitem1 h3');
-            const h3_2 = items[i].querySelector('.subitem3 h3');
-            const p = items[i].querySelector('.subitem2 p');
-            
-            h3_1.textContent = data.h3Text1[i];
-            h3_2.textContent = data.h3Text2[i];
-            p.textContent = data.pText[i];
+
+        // Limpiar el contenedor antes de actualizarlo
+        contenedorInferior.innerHTML = '';
+
+        // Crear elementos en base a los datos seleccionados
+        for (let i = 0; i < data.h3Text1.length; i++) {
+            const item = document.createElement('div');
+            item.className = 'item';
+            item.innerHTML = `
+                <div class="subitem1"><h3>${data.h3Text1[i]}</h3></div>
+                <div class="subitem2"><p>${data.pText[i]}</p></div>
+                <div class="subitem3"><h3>${data.h3Text2[i]}</h3></div>
+            `;
+            contenedorInferior.appendChild(item);
         }
-        
+
         contentSpan1.textContent = data.spanText1;
         contentSpan2.textContent = data.spanText2;
     }
 
-    optionSelect.addEventListener('change', function() {
-        const selectedOption = optionSelect.value;
+    // Manejar el cambio de selección
+    $('#mySelect').on('change', function() {
+        const selectedOption = $(this).val();
         updateContent(selectedOption);
     });
-
-    // Clonar el div de clase 'item' cinco veces
-    const itemTemplate = document.querySelector('.item');
-    for (let i = 1; i < 6; i++) {
-        const clone = itemTemplate.cloneNode(true);
-        clone.querySelector('.subitem1 h3').id = `contentH3_1_${i+1}`;
-        clone.querySelector('.subitem2 p').id = `contentP_${i+1}`;
-        clone.querySelector('.subitem3 h3').id = `contentH3_2_${i+1}`;
-        contenedorInferior.appendChild(clone);
-    }
 
     // Inicializar contenido con la opción 1 seleccionada por defecto
     updateContent('option1');
